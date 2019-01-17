d873fbf0e40283534b5811c3ee85c419b9473bf4
vagrant <vagrant@homestead>
2019-01-17 10:14:15 +0000

    Terminados ejercicios Tema 2

--- videoclub/resources/views/catalog/edit.blade.php
+++ videoclub/resources/views/catalog/edit.blade.php
@@ -2,6 +2,57 @@
 
 @section('content')
 
-	Modificar película {id}
+<div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Modificar película
+         </div>
+         <div class="card-body" style="padding:30px">
+
+            <form method="POST">
+            	{{method_field('PUT')}}
+            {{-- TODO: Protección contra CSRF --}}
+
+            <div class="form-group">
+               <label for="title">Título</label>
+               <input type="text" name="title" id="title" class="form-control" value="{{$arrayPeliculas['title']}}">
+            </div>
+
+            <div class="form-group">
+            	<label for="year">Año</label>
+            	<input type="text" name="year" id="year" class="form-control" value="{{$arrayPeliculas['year']}}">
+               {{-- TODO: Completa el input para el año --}}
+            </div>
+
+            <div class="form-group">
+            	<label for="director">Director</label>
+            	<input type="text" name="director" id="director" class="form-control" value="{{$arrayPeliculas['director']}}">
+               {{-- TODO: Completa el input para el director --}}
+            </div>
+
+            <div class="form-group">
+            	<label for="poster">Imagen</label>
+            	<input type="text" name="poster" id="poster" class="form-control" value="{{$arrayPeliculas['poster']}}">
+               {{-- TODO: Completa el input para el poster --}}
+            </div>
+
+            <div class="form-group">
+               <label for="synopsis">Resumen</label>
+               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$arrayPeliculas['synopsis']}}</textarea>
+            </div>
+
+            <div class="form-group text-center">
+               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                   Modificar Pelicula
+               </button>
+            </div>
+
+            </form>
+
+         </div>
+      </div>
+   </div>
+</div>
 
 @stop
--- videoclub/resources/views/catalog/show.blade.php
+++ videoclub/resources/views/catalog/show.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-
-	Vista detalle película {id}
+<div class="row">
+	<div class="col-xs-2 col-sm-4 col-md-3">
+        <img src="{{$arrayPeliculas['poster']}}" style="height: 250px" />
+	</div>
+	<div class="col-xs-10 col-sm-8 col-md-6">
+		<h3>
+			{{$arrayPeliculas['title']}}
+		</h3>
+		<h4>
+			Año: {{$arrayPeliculas['year']}}
+		</h4>
+		<h4>
+			Director: {{$arrayPeliculas['director']}}
+		</h4>
+		<h6>
+			<b>Resumen:</b> {{$arrayPeliculas['synopsis']}}
+		</h6>
+		<h6>
+			@if($arrayPeliculas['rented'] == false)
+				<h6><strong>Estado: </strong>Pelicula disponible</h6>
+				<button type="button" class="btn btn-info">Alquilar Pelicula</button>
+			@else
+				<h6><strong>Estado: </strong>Pelicula actualmente alquilada</h6>
+				<button type="button" class="btn btn-danger">Devolver Pelicula</button>
+			@endif
+			<a type="button" class="btn btn-warning" href="{{ url('/catalog/edit/' . $idPelicula) }}">Editar Pelicula</a>
+			<a type="button" class="btn btn-light" href="{{ url('/catalog/')}}">Volver al listado</a>
+		</h6>
+	</div>
+</div>
 
 @stop
